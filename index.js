753c23f3ab7663605b70e12231a659686137975e
christian kocevar <christian.kocevar@qualitestgroup.com>
2024-06-07 10:33:51 +0200

    woorks

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const mongoose = require("mongoose");
 const Product = require("./models/product.model.js");
 const productRoute = require("./routes/product.route.js");
 const app = express();
-
+const bodyParser =require("body-parser");
 // middleware
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
-
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
 // routes
 app.use("/api/products", productRoute);
+
+
 
 
 
@@ -19,7 +22,7 @@ app.get("/", (req, res) => {
   res.send("Hello from Node API Server Updated");
 });
 
-
+var db=mongoose.connection
 mongoose
   .connect(
    "mongodb://localhost:27017/Database"
@@ -34,3 +37,72 @@ mongoose
   .catch(() => {
     console.log("Connection failed!");
   });
+
+//form fields
+app.post("/products",(req,res) => {
+
+    var eindat= req.body.eindat
+    var entdat=req.body.entdat
+    var verdat=req.body.verdat
+    var autor=req.body.autor
+    var keyword=req.body.keyword
+    var kwp=req.body.kwp
+    var verlag=req.body.verlag
+    var seiten=req.body.seiten
+
+    var genre=req.body.genre
+    var gengenau=req.body.gengenau
+    var kontinent=req.body.kontinent
+    var land=req.body.land
+    var thema0=req.body.thema0
+    var thema1=req.body.thema1
+    var thema2=req.body.thema2
+    var zeit=req.body.zeit
+    var amalin=req.body.amalin
+
+    var position=req.body.position
+    var mark=req.body.mark
+    var markrang=req.body.markrang
+    var topw=req.body.topw
+    var belber=req.body.belber
+    var gesw=req.body.gesw
+    var zustand=req.body.zustand
+
+    var data={
+        "eindat":eindat,
+        "entdat":entdat,
+        "verdat":verdat,
+        "autor":autor,
+        "keyword":keyword,
+        "kwp":kwp,
+        "verlag":verlag,
+        "seiten":seiten,
+
+        "genre":genre,
+        "gengenau":gengenau,
+        "kontinent":kontinent,
+        "land":land,
+        "thema0":thema0,
+        "thema1":thema1,
+        "thema2":thema2,
+        "zeit":zeit,
+        "amalin":amalin,
+
+        "position":position,
+        "mark":mark,
+        "markrang":markrang,
+        "topw":topw,
+        "belber":belber,
+        "gesw":gesw,
+        "zustand":zustand
+}
+db.createCollection('products');
+db.collection('products').insertOne(data,(err,collection) => {
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Succesfully")
+    })
+}
+
+)
